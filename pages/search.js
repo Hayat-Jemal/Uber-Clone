3c1153795cdaa873477328c3eedeb4e6e756dbb9
Hayat Jemal <htjemal2000@gmail.com>
2021-11-29 04:34:07 +0300

    update

--- pages/search.js
+++ pages/search.js
@@ -3,12 +3,18 @@ import tw from "tailwind-styled-components";
 function Search() {
     return (
       <Wrapper>
-            <ButtonContainer>
-                <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png"/>
-            </ButtonContainer>
-            {/* <InputContainer>
-            
-            </InputContainer> */}
+        <ButtonContainer>
+          <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png" />
+        </ButtonContainer>
+        <InputContainer>
+          <FromToIcons>
+            <Circle src="https://img.icons8.com/ios-filled/50/9CA3AF/filled-circle.png" />
+            <Line src="https://img.icons8.com/ios/50/9CA3AF/vertical-line.png" />
+          <Square src=""/>
+                </FromToIcons>
+                <InputBoxes>
+                </InputBoxes>
+        </InputContainer>
         {/* Button container */}
         {/* input container */}
         {/* saved place */}
@@ -29,4 +35,23 @@ bg-white px-4
 // px=padding x-axis
 const BackButton = tw.img`
 h-12
+`
+
+const FromToIcons = tw.div`
+w-10 flex flex-col
+`
+const InputContainer = tw.div`
+bg-white
+`
+const Circle = tw.img`
+
+`
+const Line = tw.img`
+
+`
+const Square = tw.img`
+
+`
+const InputBoxes = te.div`
+
 `
