3d55293e9d2d95bb3deb4fb2f8787d955f979e76
Hayat Jemal <htjemal2000@gmail.com>
2021-12-12 04:01:00 +0300

    add link

--- pages/Components/RideSelector.js
+++ pages/Components/RideSelector.js
@@ -1,33 +1,38 @@
 import React, {useEffect, useState} from 'react'
 import tw from "tailwind-styled-components";
 import { carList } from '../data/carList';
-const RideSelector = () => {
-  const [RideDuration, setRideDuration] = useState(0)
+const RideSelector = ({pickupCoordinates,dropoffCoordinates}) => {
+  const [rideDuration, setRideDuration] = useState(0)
 //get ride duration from MAPBOX API
   //1,pickup coordinates
   //2,dropoffcoordinates
   useEffect(() => {
-    fetch("https://api.mapbox.com/directions/v5/mapbox/driving")
-    
-  }, [])
+    rideDuration = fetch(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${pickupCoordinates[0]},${pickupCoordinates[1]};${dropoffCoordinates[0]},${dropoffCoordinates[1]}?access_token=pk.eyJ1IjoiaGF5YXRqIiwiYSI6ImNrdzJraDJlamJyMHIybnF3MjBieGhyM2MifQ.t9i-WA_YRVJY-e6HZY-ipg`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setRideDuration(data.routes[0].duration / 100);
+      });
+  }, [pickupCoordinates, dropoffCoordinates]);
   
   return (
-      <Wrapper>
-        <Title>Choose a ride, or swipe up for more</Title>
-        <CarList>
-          {carList.map((car,index) => (
-            <Car key={index}>
-                  <CarImage src={ car.imgUrl}/>
-              <CarDetails>
-                <Service>{car.service}</Service>
-                <Time></Time>
-              </CarDetails>
-              <Price>$23</Price>
-            </Car>
-          ))}
-        </CarList>
-      </Wrapper>
-    );
+    <Wrapper>
+      <Title>Choose a ride, or swipe up for more</Title>
+      <CarList>
+        {carList.map((car, index) => (
+          <Car key={index}>
+            <CarImage src={car.imgUrl} />
+            <CarDetails>
+              <Service>{car.service}</Service>
+              <Time></Time>
+            </CarDetails>
+            <Price>{"$" + (rideDuration * car.multiplier).toFixed(2)}</Price>
+          </Car>
+        ))}
+      </CarList>
+    </Wrapper>
+  );
 }
 
 export default RideSelector
--- pages/confirm.js
+++ pages/confirm.js
@@ -3,14 +3,15 @@ import tw from "tailwind-styled-components";
 import Map from "./Components/Map";
 import { useRouter } from "next/router";
 import RideSelector from "./Components/RideSelector";
+import Link from 'next/link'
 const Confirm = () => {
   const router = useRouter();
   const { pickup, dropoff } = router.query;
   console.log("pickup", pickup);
   console.log("dropoff", dropoff);
 
-  const [pickupCoordinates, setPickupCoordinates] = useState();
-  const [dropoffCoordinates, setDropoffCoordinates] = useState();
+  const [pickupCoordinates, setPickupCoordinates] = useState([0, 0]);
+  const [dropoffCoordinates, setDropoffCoordinates] = useState([0, 0]);
 
   const getPickupgetCoordinates = (pickup) => {
     // const pickup = "Jiddah";
@@ -55,12 +56,20 @@ const Confirm = () => {
 
   return (
     <Wrapper>
+      <ButtonContainer>
+        <Link href="/search">
+        <BackButton src="https://img.icons8.com/ios-filled/50/000000/left.png"/>
+      </Link>
+      </ButtonContainer>
       <Map
         pickupCoordinates={pickupCoordinates}
         dropoffCoordinates={dropoffCoordinates}
       />
       <RideContainer>
-        <RideSelector />
+        <RideSelector
+          pickupCoordinates={pickupCoordinates}
+          dropoffCoordinates={dropoffCoordinates}
+        />
 
         <ConfirmButtonContainer>
           <ConfirmButton>Confirm UberX</ConfirmButton>
@@ -87,3 +96,10 @@ flex-1  flex flex-col h-1/2
 const Wrapper = tw.div`
 flex h-screen flex-col
 `;
+
+const ButtonContainer = tw.div`
+rounded-full absolute top-4 left-4 z-10 bg-white shadow-md cursor-pointer
+`
+const BackButton = tw.img`
+h-full object-contain
+`
